bf900a9167fe45fce418c3f1059930554f9c5d7f
dmlvr <lvr@dmlvr>
2024-02-06 18:24:58 +0200

    change Portdolio

--- components/Portfolio/Portfolio.tsx
+++ components/Portfolio/Portfolio.tsx
@@ -19,6 +19,7 @@ function Portfolio({ ruLang, darkTheme }: Props) {
         <h1>{texts.title}</h1>
         <ul className={styles.projects}>
           <li className={styles.projectCard}>
+            <h2 className={styles.title}>Каталог продукции для компании&nbsp;IEK</h2>
             <div 
               className={styles.video}
             >
@@ -34,22 +35,28 @@ function Portfolio({ ruLang, darkTheme }: Props) {
               >
                 <source src="/img/catalog-iek.mp4" type="video/mp4"/>
               </video>
-              <span>правая часть экрана за кадром потому, что запись велась с dev стенда на котором можно быстро переключаться между языками и шаблонами.</span>
             </div>
             <div className={styles.texts}>
-              <h2>Каталог продукции для&nbsp;сайта&nbsp;IEK</h2>
-              <p>Большое приложение на Next.js, отвечающее за отрисовку каталога продукции. Cамый высоконагруженный проект среди тех, которые я разрабатывал. Объем трафика постоянно растёт.</p>
+              <p>Большое web-приложение на Next.js, отвечающее за отрисовку каталога продукции. Cамый высоконагруженный проект среди тех, которые я разрабатывал. Объем трафика постоянно растёт.</p>
               <ul>
-                <li>Включает в себя более 30&nbsp;000 позиций;</li>
-                <li>Ежедневно приложением пользуются в среднем 25&nbsp;000 уникальных пользователей (объём трафика постоянно растёт);</li>
-                <li>Приложение поддерживает вывод информации на 3-х языках;</li>
-                <li>Приложение поддерживает 3 темы оформления для отрисовки на разных сайтах.</li>
-                <li>В данный момент приложение для отображения каталога используют 5 сайтов компании.</li>
+                <li>Более 30&nbsp;000 карточек товаров;</li>
+                <li>Более 25&nbsp;000 уникальных пользователей ежедневно;</li>
+                <li>Вывод информации на 3-х языках языках;</li>
+                <li>3 темы оформления.</li>
+                <li>Используется на 5 сайтах компании.</li>
               </ul>
-              <p>На проекте я отвечаю за frontend.</p>
-              <Link href='#'>Приложение на IEK</Link>
-              <Link href='#'>Приложение на ONI</Link>
-              <Link href='#'>Приложение на ITK</Link>
+              <p>На проекте я отвечаю за frontend. Посмотреть в бою можно на:</p>
+              <div className={styles.links}>
+                <Link href='https://www.iek.ru/products/catalog'>
+                  <span>IEK</span>
+                </Link>
+                <Link href='https://oni-system.com/products/catalog'>
+                  <span>ONI</span>
+                </Link>
+                <Link href='https://www.itk-group.ru/products/catalog'>
+                  <span>ITK</span>
+                </Link>
+              </div>
             </div>
           </li>
         </ul>
